83d56fe4e2a2c5ba04dd0152113f88f2dbe169f2
JustSharingSomeCode <62771039+JustSharingSomeCode@users.noreply.github.com>
2022-06-10 20:08:53 -0500

    Shake calculation according to window height

--- CSharpNation/Visualizer/CsnVisualizer.cs
+++ CSharpNation/Visualizer/CsnVisualizer.cs
@@ -162,7 +162,7 @@ namespace CSharpNation.Visualizer
                     break;
             }
 
-            float pw = WaveTools.Clamp(Power - 20, 0.0f, float.MaxValue);
+            float pw = WaveTools.Clamp(Power - (0.015f * Height), 0.0f, float.MaxValue); //ajustar
 
             Rx = (float)random.NextDouble() * (pw * 0.5f) * Dx;
             Ry = (float)random.NextDouble() * (pw * 0.5f) * Dy;
